7f52654110da811685f0c31ee9cbd49287fb6e12
haerang <gofkdvjvl@naver.com>
2019-12-30 04:11:39 +0900

    stateless Components

--- src/Ninja.js
+++ src/Ninja.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninja extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>name: {ninja.name}</div>
-          <div>age:{ninja.age}</div>
-          <div>belt: {ninja.belt}</div>
-        </div>
-      );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
-}
+// UI Component
+
+//props 전달하거나 {ninjas} 이렇게 전달 가능
+const Ninja = ({ ninjas }) => {
+  //const { ninjas } = props; //class가 아니여서 this사용 안함
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className="ninja" key={ninja.id}>
+        <div>name: {ninja.name}</div>
+        <div>age:{ninja.age}</div>
+        <div>belt: {ninja.belt}</div>
+      </div>
+    );
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninja;
