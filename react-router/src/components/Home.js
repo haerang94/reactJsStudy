bb9853938193aaba156c6e137f8baaa81cd476e7
haerang <gofkdvjvl11@gmail.com>
2020-01-06 00:28:16 +0900

    Account test

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -5,23 +5,23 @@ const Home = () => {
     <div className="container">
       <h4 className="center">Home</h4>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid animi
-        odio molestiae autem incidunt ex illo in, eveniet deleniti consequatur
-        sunt ad minima quaerat reiciendis. Sapiente commodi molestiae unde
-        doloremque! Repellendus voluptates natus aliquid, labore, minima ratione
-        exercitationem perspiciatis fugiat, temporibus perferendis sequi ullam
-        aut repellat nulla accusamus officia? Deleniti quos nulla officiis id
-        quibusdam labore voluptatem, illo culpa quam. Placeat laudantium totam
-        soluta magnam, praesentium suscipit. Temporibus cupiditate qui
-        reiciendis iusto explicabo hic quia debitis repellat eligendi maiores
-        quam perspiciatis magnam ea! Culpa quo adipisci assumenda blanditiis!
-        Alias? Voluptatibus id dicta illo itaque libero tenetur. Dolore nemo,
-        magnam officiis illo qui pariatur assumenda natus. Impedit fugit
-        praesentium saepe dicta iusto iure cum autem esse, commodi suscipit
-        omnis! Corrupti. Saepe, aperiam architecto officia consectetur magnam
-        blanditiis vitae veritatis est deleniti sed, labore earum ipsa iste
-        ullam reprehenderit aspernatur corporis tempora. Quo quos odio at ab
-        animi repudiandae sed aliquam! Dolor neque enim laudantium laborum
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
+        animiadf odio molestiae autem incidunt ex illo in, eveniet deleniti
+        consequatur sunt ad minima quaerat reiciendis. Sapiente commodi
+        molestiae unde doloremque! Repellendus voluptates natus aliquid, labore,
+        minima ratione exercitationem perspiciatis fugiat, temporibus
+        perferendis sequi ullam aut repellat nulla accusamus officia? Deleniti
+        quos nulla officiis id quibusdam labore voluptatem, illo culpa quam.
+        Placeat laudantium totam soluta magnam, praesentium suscipit. Temporibus
+        cupiditate qui reiciendis iusto explicabo hic quia debitis repellat
+        eligendi maiores quam perspiciatis magnam ea! Culpa quo adipisci
+        assumenda blanditiis! Alias? Voluptatibus id dicta illo itaque libero
+        tenetur. Dolore nemo, magnam officiis illo qui pariatur assumenda natus.
+        Impedit fugit praesentium saepe dicta iusto iure cum autem esse, commodi
+        suscipit omnis! Corrupti. Saepe, aperiam architecto officia consectetur
+        magnam blanditiis vitae veritatis est deleniti sed, labore earum ipsa
+        iste ullam reprehenderit aspernatur corporis tempora. Quo quos odio at
+        ab animi repudiandae sed aliquam! Dolor neque enim laudantium laborum
         doloremque, autem officiis vitae rerum molestias placeat tempore impedit
         Recusandae deserunt natus quae deleniti soluta! Excepturi minus non
         illo! Repudiandae magni at laboriosam labore expedita nostrum quam earum
